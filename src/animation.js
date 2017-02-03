759b205f8a82fe7ad306f30ab3b92b4d3b9bf40b
Harry Parton <hparton@hotmail.co.uk>
2017-02-03 15:33:27 +0000

    Update animation function to use Class syntax

--- src/animation.js
+++ src/animation.js
@@ -3,59 +3,85 @@
   set up like so:
 
   var anim = new Animation()
+
   anim.duration(200)
-      .easing(function(time){ return easingFunc(time) })
-      .play();
+      .easing(function(time){ return easingFunc(time) });
+
+  anim.on('tick', function(time) {
+    console.log(time)
+  })
+
+  anim.play()
 
   TODO:
-  - Add pause/resume function
+  - Add RAF shim
 
-  Version 1.0 Created by Harry Parton on 02/02/2017
+  Version 1.02 Created by Harry Parton on 03/02/2017
 */
 
-var Animation = function () {
-  this._duration = 1200
-  this._startTime
-  this._handlers = []
-  this._easingFunc = null
-  this._rafID = null
+class Animation {
+  constructor () {
+    this._duration = 1200
+    this._startedAt = null
+    this._pausedAt = null
+    this._handlers = []
+    this._easingFunc = null
+    this._rafID = null
+    this._running = true
+  }
 
   // .duration(duration) - Define a duration for the animation
-  this.duration = function (duration) {
+  duration (duration) {
     this._duration = duration
     return this
   }
 
   // .easing(easingFunc) - Define an easing function here for use in the .on('tick')
-  this.easing = function (easingFunc) {
+  easing (easingFunc) {
     this._easingFunc = easingFunc
     return this
   }
 
   // .play() - Play the animation forwards
-  this.play = function (reversed) {
-    if (!this._startTime) {
+  play (reversed) {
+    if (this._startedAt === null) {
       this._setStartTime(() => {
-        this._run(this._startTime, reversed)
+        this.trigger('start')
+        this._run(this._startedAt, reversed)
       })
     }
     return this
   }
 
   // .reverse() - Play the animation backwards
-  this.reverse = function () {
+  reverse () {
     this.play(true)
     return this
   }
 
-  // .stop() - Stop the animation, cannot be restarted
-  this.stop = function () {
+  // .pause() - Pause the animation loop but keep the RAF running so it cane be resumed.
+  pause (duration) {
+    this._running = false
+    this._pausedAt = new Date().getTime()
+    return this
+  }
+
+  // .resume() - Resume the paused animation loop.
+  resume () {
+    this._running = true
+    this._startedAt = this._startedAt + (new Date().getTime() - this._pausedAt)
+    return this
+  }
+
+  // .stop() - Stop the animation, cannot be resumed
+  stop () {
     window.cancelAnimationFrame(this._rafID)
+    this._startedAt = null
     return this
   }
 
   // .bounce(delay) - Play the animation forwards then backwards with optional delay in ms
-  this.bounce = function (delay) {
+  bounce (delay) {
     let i = 0
     this.play()
     this.on('finish', () => {
@@ -69,15 +95,29 @@ var Animation = function () {
   }
 
   // Main animation loop handler.
-  this._run = function (timestamp, reversed) {
+  _run (timestamp, reversed) {
+    /**
+     * If its paused then just skip onto the next frame.
+     * This is effectively paused as we dont send out any tick events
+     * and we never update the progress, but can be resumed. Still using
+     * resources though so may look at a better way of implementing this.
+     */
+    if (!this._running) {
+      window.requestAnimationFrame((timestamp) => { // call requestAnimationFrame again with parameters
+        this._run(timestamp, reversed)
+      })
+      return
+    }
+
+    // Otherwise do the normal run function.
     timestamp = timestamp || new Date().getTime()
-    var runtime = timestamp - this._startTime
+    var runtime = timestamp - this._startedAt
     var progress = runtime / this._duration
 
     progress = Math.min(progress, 1)
 
     if (reversed) {
-      progress = 1 - progress
+      progress = (1 - progress)
     }
 
     if (this._easingFunc !== null) {
@@ -85,21 +125,18 @@ var Animation = function () {
     }
 
     this.trigger('tick', progress)
-
     if (runtime <= this._duration) { // if duration not met yet
       this._rafID = window.requestAnimationFrame((timestamp) => { // call requestAnimationFrame again with parameters
         this._run(timestamp, reversed)
       })
     } else {
-      this._startTime = undefined
+      this._startedAt = null
       this.trigger('finish')
     }
-
-    return this
   }
 
   // .trigger('tick'/'finish') - Trigger any callbacks assigned to the event
-  this.trigger = function (event, params) {
+  trigger (event, params) {
     for (var i = 0; i < this._handlers.length; i++) {
       if (this._handlers[i].event === event) {
         this._handlers[i].cb(params)
@@ -108,22 +145,22 @@ var Animation = function () {
   }
 
   // .on('tick'/'finish') - Assign a callback to the event
-  this.on = function (event, cb) {
+  on (event, cb) {
     this._handlers.push({event, cb})
     return this
   }
 
   // Does what it says on the tin.
-  this._setStartTime = function (cb) {
+  _setStartTime (cb) {
     window.requestAnimationFrame((timestamp) => {
       // if browser doesn't support requestAnimationFrame, generate our own timestamp using Date
-      this._startTime = timestamp || new Date().getTime()
+      this._startedAt = timestamp || new Date().getTime()
       cb(timestamp)
     })
   }
 
   // .log() - Log the animation object
-  this.log = function () {
+  log () {
     console.log(this)
     return this
   }
